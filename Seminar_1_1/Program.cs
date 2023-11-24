d84df1efef5f6b49e1a82ca975b123ecb8ff00c6
vitvit_x <ykropchik2283@gmail.com>
2023-11-24 11:31:01 +0300

    Задача  программа которая принимает на вход 2 числа и проверяет является первое число квадратом второго

---
+++ Seminar_1_1/Program.cs
@@ -0,0 +1,18 @@
+﻿
+Console.WriteLine("введите число1 ");
+int FirstNumber = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("введите число2 ");
+int SecondNumber = Convert.ToInt32(Console.ReadLine());
+
+if(FirstNumber == SecondNumber * SecondNumber)
+{
+    System.Console.WriteLine($"a = {FirstNumber}, b = {SecondNumber} -> Да");
+}
+else
+{   
+    System.Console.WriteLine($"a = {FirstNumber}, b = {SecondNumber} -> Нет");
+}
+
+// = -Присвоить значение
+// == - Сравнение
