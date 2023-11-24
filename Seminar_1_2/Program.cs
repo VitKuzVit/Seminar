b859079dcedba4ce47c831b08642934b5a8f5ee7
vitvit_x <ykropchik2283@gmail.com>
2023-11-24 13:06:56 +0300

    Программа которая принимает одно число, а на выходе показывает все целые числа в промежутке от -N до N

---
+++ Seminar_1_2/Program.cs
@@ -0,0 +1,30 @@
+﻿
+// Console.WriteLine("number");
+// double numberA = Convert.ToInt32(Console.ReadLine());
+// double index = numberA;
+// int count = 0;
+// numberA = -numberA;
+// while (count <= (index * 2))
+// {
+//     System.Console.WriteLine(numberA);
+//     numberA++;
+//     count++;
+// }
+
+
+Console.WriteLine("number");
+int N = Convert.ToInt32(Console.ReadLine());
+if (N > 0)
+{
+int I = -N;
+
+while (I <= N)
+{
+    System.Console.WriteLine(I + " ");
+    I++;
+}
+}
+else
+{
+    System.Console.WriteLine("Введено не коректное значение");
+}
