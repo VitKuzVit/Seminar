2e129f281129f8254ac8e8a9b52c23750d59e3ce
vitvit_x <ykropchik2283@gmail.com>
2023-11-24 10:40:11 +0300

    Задача 1 программа которая принимает на вход число и выдает его квадрат

---
+++ Seminar_1/Program.cs
@@ -0,0 +1,12 @@
+﻿// Пригласили пользователя кл ввду
+
+
+Console.Write("Ведите число ");
+// int namber = int.Parse(Console.ReadLine());
+int number = Convert.ToInt32(Console.ReadLine());
+
+// Получили квадрат числа и записали его в переменную SQR
+int sqr = number * number;
+
+// Console.WriteLine(number + " -> " + sqr); вывод текста (подсказки) в терминал
+System.Console.WriteLine($"{number} -> {sqr} текс текст");
