80f5f43a2f250110fc0d3b5add979eedd0d3cc79
vitvit_x <ykropchik2283@gmail.com>
2023-11-25 12:34:42 +0300

    Деление с остатком

---
+++ Seminae_1_3/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Globalization;
+
+System.Console.WriteLine("Введите трехзначное число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if(number >= 100 && number <= 999)
+{
+    System.Console.WriteLine(number%10);
+}
+else
+{
+    System.Console.WriteLine("Вы ввели не трехзначное число");
+}
