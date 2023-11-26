e00826f6a71154922ae1a87f897c1f72c7dfd26e
vitvit_x <ykropchik2283@gmail.com>
2023-11-26 15:47:17 +0300

    создали Функции FindNumber

--- Seminar_2_1/Program.cs
+++ Seminar_2_1/Program.cs
@@ -26,9 +26,27 @@ void PrintArray(int[] array)
 }
 // Функция void с названием PrinArray выводит на экран некий Массив
 
+void FindNumber(int[] array, int N)
+{
+    for(int i = 0; i < array.Length; i++)
+    {
+        if(array[i] == N)
+        {
+            System.Console.WriteLine("Да");
+            return;
+        }
+    }
+
+    System.Console.WriteLine("Нет");
+}
+// Функция void с названием FindNumber внутри с циклом for
+
 // Основной код
 int size = ReadInt("Введите размер массива ");
 // задаем значение переменной size обращаясь к функции ReadInt
 int[] myArray = GenerateArray(size, -10, 10);
 // Создаем массив с имененм myArray размером size, внутри наполненным значением целых чисел от -10 до +10.
 PrintArray(myArray);
+// выводим на экран массив
+int number = ReadInt("Введите искомое число: ");
+FindNumber(myArray, number);
