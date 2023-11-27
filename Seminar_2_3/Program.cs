a56ca79003284022cda8897c353e35b3cbbde3a3
vitvit_x <ykropchik2283@gmail.com>
2023-11-27 15:34:15 +0300

    Умножение пар массива и создание нового массива с производными

---
+++ Seminar_2_3/Program.cs
@@ -0,0 +1,13 @@
+﻿int[] Array = new int [6] {1, 3, 2, 4, 2, 3};
+
+int[] newArray = new int[3];
+
+for (int i = 0; i < 3; i++)
+{
+    newArray[i] = Array[i] * Array[Array.Length-1-i];
+}
+void PrintArray(int[] array)
+{
+    System.Console.WriteLine("[" + string.Join(", ", array) + "]");
+}
+PrintArray(newArray);
