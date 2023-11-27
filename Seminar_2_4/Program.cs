83fabc97bc94c544d2fd480b0d3c9d14674bd936
vitvit_x <ykropchik2283@gmail.com>
2023-11-27 15:45:16 +0300

    Заполнение массива дробными числами

---
+++ Seminar_2_4/Program.cs
@@ -0,0 +1,8 @@
+﻿double[] array = new double[5];
+Random rand = new Random();
+for(int i = 0; i < array.Length; i++)
+{
+    array[i] = Math.Round(rand.Next(0, 10) + rand.NextDouble(), 2);
+}
+
+System.Console.WriteLine(string.Join(" ", array));
